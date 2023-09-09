409eb620c1eaa5a6415fe22313eb19cce9b61c82
PucKu <komovk@gmail.com>
2023-09-10 01:14:43 +0300

    Добавлены решения для домашнего задания 2 семинара

---
+++ Семинар 2. Базовые алгоритмы/Задача 10/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int ConsoleReading (string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+bool NuberCheck (int number)
+{
+    return number >= 100 && number <= 999;
+}
+
+void FindSecondNumber (int N)
+{
+    if (NuberCheck(N))
+    {
+        int secondNumber = (N / 10) % 10;
+        Console.WriteLine("Вторая цифра числа " + N + " равна " + secondNumber);
+    }
+    else
+    {
+        Console.WriteLine("Число должно быть трехзначным!");
+    }
+}
+
+int n = ConsoleReading("Ввидите трехзначное число: ");
+FindSecondNumber(n);
