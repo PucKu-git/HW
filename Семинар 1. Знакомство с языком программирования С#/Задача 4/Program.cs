001886d40778c474cc1a3cbc72dc17275a0896b0
PucKu <komovk@gmail.com>
2023-09-09 09:33:31 +0300

    Добавлены решения для домашнего задания 1 семинара

---
+++ Семинар 1. Знакомство с языком программирования С#/Задача 4/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числа
+и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.Write("Введите первое число: ");
+int first = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int second = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число: ");
+int third = int.Parse(Console.ReadLine()!);
+
+
+if (first > second && first > third)
+{
+     Console.WriteLine("Первое число (" + first + ") - максимальное");
+}
+else if (second > third)
+{
+    Console.WriteLine("Второе число (" + second + ") - максимальное");
+}
+else
+{
+    Console.WriteLine("Третье число (" + third + ") - максимальное");
+}
